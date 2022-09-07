29b2812ac04828606d549f7bd23dc45676e4f450
1vijit <111475826+1vijit@users.noreply.github.com>
2022-09-07 14:27:52 -0400

    working code

--- main.py
+++ main.py
@@ -4,4 +4,28 @@ from money_machine import MoneyMachine
 
 
 money_machine = MoneyMachine()
-money_machine.report()
+coffee_maker = CoffeeMaker()
+menu=Menu()
+
+is_on = True
+
+
+while is_on:
+    options=menu.get_items()
+    choice=input(f"What would you like to have ({options}): ")
+    if choice == "off":
+        is_on=False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choice)
+        can_make=coffee_maker.is_resource_sufficient(drink)
+        price=drink.cost
+        print(f"price of {choice} is {price}")
+        if can_make:
+            # total_money=money_machine.process_coins()
+            payment=money_machine.make_payment(price)
+            if payment:
+                coffee_maker.make_coffee(drink)
+
